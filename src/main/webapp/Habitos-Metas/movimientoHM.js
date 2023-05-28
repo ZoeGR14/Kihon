f2af49cd9a52057f37486a5860a635dc02d2afac
= <lcontrerasd2000@alumno.ipn.mx>
2023-05-27 21:13:14 -0600

    ayuda con el amrgin

---
+++ src/main/webapp/Habitos-Metas/movimientoHM.js
@@ -0,0 +1,136 @@
+const movPag = document.querySelector(".movPag");
+const btn_adelante2 = document.querySelector(".sigPag");
+
+const btn_atras1 = document.querySelector(".volver-pag1");
+const btn_adelante3 = document.querySelector(".adelante-pag3");
+
+const btn_atras2 = document.querySelector(".volver-pag2");
+const btn_adelante4 = document.querySelector(".adelante-pag4");
+
+const btn_atras3 = document.querySelector(".volver-pag3");
+const btn_adelante5 = document.querySelector(".adelante-pag5");
+
+const btn_atras4 = document.querySelector(".volver-pag4");
+const btn_adelante6 = document.querySelector(".adelante-pag6");
+
+const btn_atras5 = document.querySelector(".volver-pag5");
+const btn_adelante7 = document.querySelector(".adelante-pag7");
+
+const btn_atras6 = document.querySelector(".volver-pag6");
+const btn_adelante8 = document.querySelector(".adelante-pag8");
+
+const btn_atras7 = document.querySelector(".volver-pag7");
+const btn_adelante9 = document.querySelector(".adelante-pag9");
+
+const btn_atras8 = document.querySelector(".volver-pag8");
+const btn_adelante10 = document.querySelector(".adelante-pag10");
+
+const btn_atras9 = document.querySelector(".volver-pag9");
+const btn_adelante11 = document.querySelector(".adelante-pag11");
+
+const btn_atras10 = document.querySelector(".volver-pag10");
+const btn_adelante12 = document.querySelector(".adelante-pag12");
+
+const btn_atras11 = document.querySelector(".volver-pag11");
+const btn_adelante13 = document.querySelector(".adelante-pag13");
+
+const btn_atras12 = document.querySelector(".volver-pag12");
+const btn_adelante14 = document.querySelector(".adelante-pag14");
+
+const btn_atras13 = document.querySelector(".volver-pag13");
+const btn_adelante15 = document.querySelector(".adelante-pag15");
+
+const btn_atras14 = document.querySelector(".volver-pag14");
+const btn_final = document.querySelector(".fin");
+
+let max = 15;
+let cont = 1;
+
+
+
+btn_adelante2.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-calc(300px + 6.6%)";
+    
+    
+});
+
+btn_adelante3.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-13.2%";
+});
+
+btn_adelante4.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-19.8%";
+});
+
+btn_adelante5.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-26.4%";
+});
+
+btn_adelante6.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-33%";
+});
+
+btn_adelante7.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-39.6%";
+});
+
+btn_adelante8.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-46.2%";
+});
+
+btn_adelante9.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-52.8%";
+});
+
+btn_adelante10.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-59.4%";
+});
+
+btn_adelante11.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-66%";
+});
+
+btn_adelante12.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-72.6%";
+});
+
+btn_adelante13.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-79.2%";
+});
+
+btn_adelante14.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-85.8%";
+});
+
+btn_adelante15.addEventListener("click", function (e){
+    e.preventDefault;
+    
+    movPag.style.marginLeft = "-92.4%";
+});
+
